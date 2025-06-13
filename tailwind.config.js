540735eed09edbb8ee36d971aa3602f6089ef1ef
Rith_Cool <32011081+narithtyty@users.noreply.github.com>
2025-06-13 14:42:42 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'noto-thai': ['"Noto Sans Thai"', 'sans-serif'],
+        'sans': ['"Noto Sans Thai"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-} 
+}
